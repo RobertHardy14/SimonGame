5e086c55f00f0f3ae5d9bb8e79ad32b55f8376de
RobertHardy14 <rob.moreno@hotmail.com>
2021-02-07 18:35:39 -0600

    Made js work, added sounds

--- game.js
+++ game.js
@@ -2,11 +2,29 @@ var buttonColors = ["red", "blue", "green", "yellow"]
 
 var gamePattern = []
 
+var userClickedPattern = []
+
 function nextSequence() {
-    randomNumber = console.log(Math.floor(Math.random() * 4))
+   var randomNumber = Math.floor(Math.random() * 4)
     return randomNumber
 }
 
 var randomChosenColor = buttonColors[nextSequence()]
 
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100)
+gamePattern.push(randomChosenColor)
+console.log(gamePattern)
+
+playSound(randomChosenColor) 
+$("#" + randomChosenColor).animate({opacity: 0.2}, "fast").animate({opacity: 1}, "fast") 
+
+$("div[type='button']").click(function () {
+    var userChosenColor = this.id
+    userClickedPattern.push(userChosenColor)
+    playSound(userChosenColor)
+    console.log(userClickedPattern)
+})
+
+function playSound(name) {
+    var sound = new Audio("sounds/" + name + ".mp3")
+    sound.play()
+}
